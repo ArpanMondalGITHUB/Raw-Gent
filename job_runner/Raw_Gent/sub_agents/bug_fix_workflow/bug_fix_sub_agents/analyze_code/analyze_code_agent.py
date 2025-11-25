436ad0267a33693081e1bc71d31a011a4cb63b8b
Arpan Mondal <arpanmondal572@gmail.com>
2025-11-25 11:51:04 +0530

    fix: fixed import file path and init files

--- job_runner/Raw_Gent/sub_agents/bug_fix_workflow/bug_fix_sub_agents/analyze_code/analyze_code_agent.py
+++ job_runner/Raw_Gent/sub_agents/bug_fix_workflow/bug_fix_sub_agents/analyze_code/analyze_code_agent.py
@@ -1,11 +1,9 @@
 from google.adk.agents import LlmAgent
-from analyze_code import analyze_code_prompt
-from core.config import GEMINI_API_KEY
+from . import analyze_code_prompt
 
 Analyze_Code_Agent = LlmAgent(
     name="analyze_code",
     model="gemini-2.0-flash",
-    model_param = GEMINI_API_KEY,
     description="",
     instruction=analyze_code_prompt.Analyze_Code_Prompt
 )
